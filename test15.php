a3ca2f03ddfcf6e5624f342de861a8de4956e3a5
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-05-27 13:49:42 +0900

    file_put_contents

--- test15.php
+++ test15.php
@@ -1,11 +1,14 @@
 <?php
 // 1. 파일을 여는데 모드 체크해서 열기
 $fp = fopen("write.txt", "w");
-$str = "Marry has a dog";
+$str = "Marry has a cat";
 
 // 2. 파일에 $str을 쓴다.
-fwrite($fp, $str);
+//  fwrite($fp, $str);
+file_put_contents("write2.txt", $str);
 
 // 3. 파일을 닫는다.
 fclose($fp);
+
+echo "saved.";
 ?>
